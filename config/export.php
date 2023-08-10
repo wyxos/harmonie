eff93877ee13f13f17d72960f2c11ef178519a44
Joey J <joey.jaulimsing@live.com>
2023-08-10 19:12:39 +0400

    chore: dynamic queue name

--- config/export.php
+++ config/export.php
@@ -5,5 +5,6 @@ return [
     'job' => \Wyxos\Harmonie\Export\Jobs\ExportRecords::class,
     'calculate' => \Wyxos\Harmonie\Export\Jobs\CalculateChunks::class,
     'event' => \Wyxos\Harmonie\Export\Events\ExportUpdate::class,
-    'base' => \Wyxos\Harmonie\Export\ExportBase::class
+    'base' => \Wyxos\Harmonie\Export\ExportBase::class,
+    'queue' => 'export'
 ];
