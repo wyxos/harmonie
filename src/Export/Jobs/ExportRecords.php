fc136f4a702df1463f52b2088eafb5114f9a29fb
Joey J <joey.jaulimsing@lawcreative.co.uk>
2024-10-24 20:16:01 +0400

    Release v1.3.7

--- src/Export/Jobs/ExportRecords.php
+++ src/Export/Jobs/ExportRecords.php
@@ -53,7 +53,7 @@ class ExportRecords implements ShouldQueue
             return;
         }
 
-        $instance = new $this->instance((array)$this->export->parameters);
+        $instance = new $this->instance($this->export->parameters);
 
         $this->export->update([
             'status' => 'processing'
