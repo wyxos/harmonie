2931dfdacde094cd45882c643885d18e6974e4af
Joey J <joey@wyxos.com>
2025-06-09 00:37:26 +0400

    feat: add dynamic registration for Import and Export service providers and enhance setup command

--- src/Harmonie/Commands/Setup.php
+++ src/Harmonie/Commands/Setup.php
@@ -13,12 +13,47 @@ class Setup extends Command
 
     public function handle()
     {
-        if ($this->confirm('Do you want to set up Harmonie exports?', true)) {
+        $enableExport = $this->confirm('Do you want to enable Harmonie exports?', true);
+
+        if ($enableExport) {
             $this->call('harmonie:setup-exports');
+            $this->updateConfig('export', true);
+            $this->info('Exports have been enabled in the configuration.');
+        } else {
+            $this->updateConfig('export', false);
+            $this->info('Exports have been disabled in the configuration.');
         }
 
-        if ($this->confirm('Do you want to set up Harmonie imports?', true)) {
+        $enableImport = $this->confirm('Do you want to enable Harmonie imports?', true);
+
+        if ($enableImport) {
             $this->call('harmonie:setup-imports');
+            $this->updateConfig('import', true);
+            $this->info('Imports have been enabled in the configuration.');
+        } else {
+            $this->updateConfig('import', false);
+            $this->info('Imports have been disabled in the configuration.');
         }
+
+        $this->call('vendor:publish', [
+            '--tag' => 'harmonie:harmonie-config',
+            '--force' => true
+        ]);
+
+        $this->info('Configuration has been published.');
+    }
+
+    protected function updateConfig($feature, $enabled)
+    {
+        $configPath = config_path('harmonie.php');
+        $config = file_get_contents($configPath);
+
+        // Update the config value
+        $pattern = "/'$feature' => " . ($enabled ? 'false' : 'true') . "/";
+        $replacement = "'$feature' => " . ($enabled ? 'true' : 'false');
+
+        $config = preg_replace($pattern, $replacement, $config);
+
+        file_put_contents($configPath, $config);
     }
 }
